d179d33a5b937f5b37fc8b883b36b6b1067c87f9
Muhammadbilol <muhammadbilolmamasidiqov316@gmail.com>
2026-01-18 12:13:36 +0500

    fhfshshs

---
+++ js/login.js
@@ -0,0 +1,32 @@
+const elLoginForm = document.getElementById("loginForm");
+elLoginForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const formData = new FormData(elLoginForm);
+  const result = {};
+
+  formData.forEach((value, key) => {
+    result[key] = value;
+  });
+
+  login(result);
+});
+
+function login(data) {
+  fetch("https://json-api.uz/api/project/fn44-amaliyot/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      localStorage.setItem("token", res.access_token);
+      location.href = "./index.html";
+    })
+    .catch(() => {
+      alert("Parol Yoki Login Xato ❌");
+    });
+}
